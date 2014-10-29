344521f979afbfbf717ca85730dcb50820b6b6d5
Marc Tönsing <marc@eatsmarter.de>
2014-10-29 16:11:31 -0700

    Initial commit.
    Just testing to connect github to wordpress svn

---
+++ jquery.marctv-ajax-trash-comments.js
@@ -0,0 +1,33 @@
+jQuery(function($) {
+  $('a.marctv-ajax-trash').click(function() {
+    
+    var cid = $(this).data('cid');
+    var nonce = $(this).data('nonce');
+
+    function getData() {
+      return $.ajax({
+        type: 'POST',
+        url: marctvedc.adminurl,
+        data: {
+          action: 'marctv_trash_comment',
+          cid: cid,
+          _wpnonce: nonce
+        },
+        dataType: 'html'
+      });
+    }
+
+    function handleData(data /* , textStatus, jqXHR */) {
+      if (data > -1) {
+        $('#li-comment-' + cid).fadeOut('slow');
+      } else {
+         $('a.marctv-ajax-trash','#li-comment-' + cid).after(' <em class="marctv-error" data-error="' + data + '">error</em>');
+      }
+    }
+
+    getData().done(handleData);
+
+    return false;
+  });
+});
+
---
+++ readme.txt
@@ -0,0 +1,17 @@
+=== MarcTV Easy Delete Comments  ===
+Contributors: MarcDK
+Tags: MarcTV, comments
+Requires at least: 3.0
+Tested up to: 4.0
+Stable tag: 1.0
+
+== Description ==
+
+Removes comments easier.
+
+== Installation ==
+
+* Install plugin
+* Activate it
+
+== Changelog ==
