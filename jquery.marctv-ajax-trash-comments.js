c0f135a741a11d28d30097577563349c82617168
Marc Tönsing <marc@eatsmarter.de>
2014-10-29 17:52:48 -0700

    Changed jQuery action to a message instead of an animation.

--- jquery.marctv-ajax-trash-comments.js
+++ jquery.marctv-ajax-trash-comments.js
@@ -1,31 +1,27 @@
 jQuery(function($) {
   $('a.marctv-trash').click(function() {
-    
+    var trashlink = $(this);
     var cid = $(this).data('cid');
     var nonce = $(this).data('nonce');
 
-    function getData() {
-      return $.ajax({
-        type: 'POST',
-        url: marctvedc.adminurl,
-        data: {
-          action: 'marctv_trash_comment',
-          cid: cid,
-          _wpnonce: nonce
-        },
-        dataType: 'html'
-      });
-    }
+    $.ajax({
+      type: 'POST',
+      url: marctvedc.adminurl,
+      data: {
+        action: 'marctv_trash_comment',
+        cid: cid,
+        _wpnonce: nonce
+      },
+      success: function(data) {
+        if (data > -1) {
+          $(trashlink).after(' <em class="marctv-ok" data-error="' + data + '">trashed!</em>');
+        } else {
+          $(trashlink).after(' <em class="marctv-error" data-error="' + data + '">error</em>');
+        }
+      },
+      dataType: 'html'
+    });
 
-    function handleData(data /* , textStatus, jqXHR */) {
-      if (data > -1) {
-        $('#li-comment-' + cid).fadeOut('slow');
-      } else {
-         $('a.marctv-trash','#li-comment-' + cid).after(' <em class="marctv-error" data-error="' + data + '">error</em>');
-      }
-    }
-
-    getData().done(handleData);
 
     return false;
   });
