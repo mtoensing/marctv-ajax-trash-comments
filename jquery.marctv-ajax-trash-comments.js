14b8fa03bfe8a4ec1975b427c5459e18922f5462
Marc Tönsing <marc@eatsmarter.de>
2014-10-31 16:57:14 -0700

    CHanged ajax button status when clicked. Simplified ajax error handling.

--- jquery.marctv-ajax-trash-comments.js
+++ jquery.marctv-ajax-trash-comments.js
@@ -1,29 +1,29 @@
-jQuery(function($) {
-  $('a.marctv-trash').click(function() {
-    var trashlink = $(this);
-    var cid = $(this).data('cid');
-    var nonce = $(this).data('nonce');
+jQuery(function ($) {
+    $('.marctv-trash').click(function () {
+        var trashlink = $(this);
+        var cid = $(this).data('cid');
+        var nonce = $(this).data('nonce');
 
-    $.ajax({
-      type: 'POST',
-      url: marctvedc.adminurl,
-      data: {
-        action: 'marctv_trash_comment',
-        cid: cid,
-        _wpnonce: nonce
-      },
-      success: function(data) {
-        if (data > -1) {
-          $(trashlink).after(' <em class="marctv-ok" data-error="' + data + '">trashed!</em>');
-        } else {
-          $(trashlink).after(' <em class="marctv-error" data-error="' + data + '">error</em>');
-        }
-      },
-      dataType: 'html'
+        $.ajax({
+            type: 'POST',
+            url: marctvedc.adminurl,
+            data: {
+                action: 'marctv_trash_comment',
+                cid: cid,
+                _wpnonce: nonce
+            },
+            success: function (data) {
+                if (data == 'error' || data == -1) {
+                    $(trashlink).addClass('marctv-error').text('Error!');
+                } else {
+                    $(trashlink).addClass('marctv-ok').text(data);
+                }
+            },
+            dataType: 'html'
+        });
+
+
+        return false;
     });
-
-
-    return false;
-  });
 });
 
--- marctv-ajax-trash-comments.php
+++ marctv-ajax-trash-comments.php
@@ -17,55 +17,70 @@
 
  */
 
-function get_trash_comment_link($comment_text) {
-  if (current_user_can('moderate_comments')) {
-    $comment_id = get_comment_ID();
-    $nonce = wp_create_nonce("delete-comment_$comment_id");
-    $del_nonce = esc_html('_wpnonce=' . $nonce);
-    $trash_url = esc_url("/wp-admin/comment.php?action=trashcomment&c=$comment_id&$del_nonce");
-    $trash_link = "<small><a data-nonce='$nonce' data-cid='$comment_id' class='marctv-trash' href='$trash_url' title='" . esc_attr__(__('Move this comment to the trash', 'marctv-ajax-trash-comments')) . "'>" . __('trash', 'marctv-ajax-trash-comments') . '</a></small>';
+function get_trash_comment_link( $comment_text ) {
+	if ( current_user_can( 'moderate_comments' ) ) {
+		$comment_id = get_comment_ID();
+		$nonce      = wp_create_nonce("delete-comment_$comment_id");
+		$del_nonce  = esc_html( '_wpnonce=' . $nonce );
+		$trash_url  = esc_url( "/wp-admin/comment.php?action=trashcomment&c=$comment_id&$del_nonce" );
+		$trash_link = "<small><a data-nonce='$nonce' data-cid='$comment_id' class='marctv-trash' href='$trash_url' title='" . esc_attr__( __( 'Move this comment to the trash', 'marctv-ajax-trash-comments' ) ) . "'>" . __( 'trash', 'marctv-ajax-trash-comments' ) . '</a></small>';
 
-    return $comment_text . $trash_link;
-  }
+		return $comment_text . $trash_link;
+	}
 }
 
 function add_marctv_ajax_comment_scripts() {
-  wp_enqueue_style(
-      "jquery.marctv_edc", WP_PLUGIN_URL .  "/" . dirname(plugin_basename(__FILE__)) . "/marctv-ajax-trash-comments.css", false, "1.0");
+	wp_enqueue_style(
+		"jquery.marctv_edc", WP_PLUGIN_URL . "/" . dirname( plugin_basename( __FILE__ ) ) . "/marctv-ajax-trash-comments.css", false, "1.0" );
 
-  wp_enqueue_script(
-      "jquery.marctv_edc", WP_PLUGIN_URL . "/" .  dirname(plugin_basename(__FILE__)) . "/jquery.marctv-ajax-trash-comments.js", array("jquery"), "1.0", 0);
+	wp_enqueue_script(
+		"jquery.marctv_edc", WP_PLUGIN_URL . "/" . dirname( plugin_basename( __FILE__ ) ) . "/jquery.marctv-ajax-trash-comments.js", array( "jquery" ), "1.0", 0 );
 
-  $params = array(
-    'adminurl' => admin_url('admin-ajax.php'),
-  );
+	$params = array(
+		'adminurl' => admin_url( 'admin-ajax.php' ),
+	);
 
-  wp_localize_script('jquery.marctv_edc', 'marctvedc', $params);
+	wp_localize_script( 'jquery.marctv_edc', 'marctvedc', $params );
 }
 
 function marctv_trash_comment() {
 
-  if (current_user_can('moderate_comments')) {
-    /* retrieve comment id from post ajax request and sanitize to number */
-    $comment_id = filter_input(INPUT_POST, 'cid', FILTER_SANITIZE_NUMBER_FLOAT);
+	if ( current_user_can( 'moderate_comments' ) ) {
+		/* retrieve comment id from post ajax request and sanitize to number */
+		$comment_id = filter_input( INPUT_POST, 'cid', FILTER_SANITIZE_NUMBER_FLOAT );
+		$comment_status = wp_get_comment_status( $comment_id );
 
-    /* check if the ajax referer is the trash link */
-    check_ajax_referer("delete-comment_$comment_id");
+		/* check if the ajax referrer is the trash link */
+		check_ajax_referer("delete-comment_$comment_id");
 
-    /* move comment to the trash */
-    wp_trash_comment($comment_id);
-  } 
-  
+		switch ( $comment_status ) {
+			case 'approved':
+				if( wp_trash_comment( $comment_id ) ) {
+					wp_die(  __( 'untrash', 'marctv-ajax-trash-comments' ) );
+				}
+
+				break;
+			case 'trash':
+				if( wp_untrash_comment( $comment_id ) ) {
+					wp_die( __( 'trash', 'marctv-ajax-trash-comments' ) );
+				}
+				break;
+			default:
+				wp_die( 'error' );
+		}
+
+	}
+
 }
 
 function marctv_ajax_trash_comments_load_textdomain() {
-  load_plugin_textdomain('marctv-ajax-trash-comments', false, dirname(plugin_basename(__FILE__)) . '/languages/');
+	load_plugin_textdomain( 'marctv-ajax-trash-comments', false, dirname( plugin_basename( __FILE__ ) ) . '/languages/' );
 }
 
-add_filter('comment_text', 'get_trash_comment_link', 99);
+add_filter( 'comment_text', 'get_trash_comment_link', 99 );
 
-add_action('wp_print_styles', 'add_marctv_ajax_comment_scripts');
+add_action( 'wp_print_styles', 'add_marctv_ajax_comment_scripts' );
 
-add_action("wp_ajax_marctv_trash_comment", "marctv_trash_comment");
+add_action( "wp_ajax_marctv_trash_comment", "marctv_trash_comment" );
 
-add_action('plugins_loaded', 'marctv_ajax_trash_comments_load_textdomain');
+add_action( 'plugins_loaded', 'marctv_ajax_trash_comments_load_textdomain' );
